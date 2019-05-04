8d58a37bbddfa42d53bec4dfb902395012fe9b32
Vladimir <nekrasowladimir@gmail.com>
2019-05-04 13:45:51 +0930

    Added Docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.coverage',
     'sphinx.ext.napoleon',
+    'sphinx.ext.githubpages',
     'm2r']
 
 # Add any paths that contain templates here, relative to this directory.
