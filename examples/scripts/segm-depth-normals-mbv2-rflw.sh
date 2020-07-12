a83fe19556abc3e36ddf61fd7a898643f090530f
Vladimir <nekrasowladimir@gmail.com>
2020-07-13 09:12:05 +0930

    Added default script for 3-task training

---
+++ examples/scripts/segm-depth-normals-mbv2-rflw.sh
@@ -0,0 +1,20 @@
+python src/train.py \
+    --tasks segm depth normals \
+    --enc-backbone "mobilenetv2" \
+    --enc-return-layers 1 2 3 4 5 6 \
+    --dec-combine-layers "[[0, 1], [2, 3], 4, 5]" \
+    --num-classes 40 1 3 \
+    --ignore-indices 255 0 1 \
+    --train-list-path ./src/data/train_list3.txt \
+    --val-list-path ./src/data/val_list3.txt \
+    --train-dir ./src/datasets/nyudv2/ \
+    --val-dir ./src/datasets/nyudv2/ \
+    --val-every 5 \
+    --depth-scale 5000.0 \
+    --initial-values 0.0 10000.0 10000.0 \
+    --saving-criterions "up" "down" "down" \
+    --tasks-loss-weights 0.5 0.5 5.0 \
+    --enc-lr 1e-3 \
+    --dec-lr 1e-2 \
+    --ckpt-dir "./checkpoints-mbv2-sdn/" \
+    --ckpt-path "./checkpoint.pth.tar2" \
